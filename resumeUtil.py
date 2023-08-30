8bfc315e0e0d84dc7a4bf15809a975bd0f2c34e9
yuval-krn <yk2917@columbia.edu>
2023-08-30 15:14:34 -0700

    Refactor for readability

--- resumeUtil.py
+++ resumeUtil.py
@@ -3,19 +3,29 @@ import datetime
 import shutil
 import os
 
-## generate new file name
-current_time = datetime.datetime.now()
-base = "YuvalKerenResume"
-full = base + str(current_time.month) + "-" + str(current_time.day) + "-" + str(current_time.year)
-full += "-" + str(current_time.hour) + ":" + str(current_time.minute) + ".pdf"
-print(full)
+def generate_new_filename(base):
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%m-%d-%Y-%H:%M")
+    new_filename = f"{base}_{formatted_time}.pdf"
+    return new_filename
 
-## copy current resume pdf into OldResumes folder w/ new name
-script_directory = os.path.dirname(__file__)
-src = os.path.join(script_directory, "..", base + ".pdf")
-dest = os.path.join(script_directory, "..", "OldResumes", full)
+def copy_resume_to_old_resumes(source_path, destination_directory, new_filename):
+    dest_path = os.path.join(destination_directory, new_filename)
+    shutil.copy(source_path, dest_path)
 
-shutil.copy(src, dest)
+def main():
+    base_filename = "YuvalKerenResume"
+    script_directory = os.path.dirname(__file__)
 
-## replace YuvalKerenResume with new .docx version
-convert("../" + base + ".docx", "../" + base + ".pdf")
+    source_resume_path = os.path.join(script_directory, "..", f"{base_filename}.pdf")
+    destination_resumes_directory = os.path.join(script_directory, "..", "OldResumes")
+
+    new_resume_filename = generate_new_filename(base_filename)
+    copy_resume_to_old_resumes(source_resume_path, destination_resumes_directory, new_resume_filename)
+
+    docx_path = os.path.join(script_directory, "..", f"{base_filename}.docx")
+    pdf_path = os.path.join(script_directory, "..", f"{base_filename}.pdf")
+    convert(docx_path, pdf_path)
+
+if __name__ == "__main__":
+    main()
